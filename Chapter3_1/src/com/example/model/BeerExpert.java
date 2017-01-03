7fd47fed90b688654add4b9719b0860da6179225
Andrius <andriusjavait@gmail.com>
2017-01-03 20:33:55 +0000

    Update BeerExpert code with one more method to getAllSize in List of String

--- Chapter3_1/src/com/example/model/BeerExpert.java
+++ Chapter3_1/src/com/example/model/BeerExpert.java
@@ -18,9 +18,9 @@ public class BeerExpert  {
     }
 	// this method just get longer answer more precise when you select from form
 	// it give you one answer size is ...
-	public String getOneSize(String body) {
-        String result = body;
-        switch (body) {
+	public String getOneSize(String size) {
+        String result = size;
+        switch (size) {
             case "12oz": {
                 result = "size is 12";
                 break;
@@ -34,5 +34,20 @@ public class BeerExpert  {
             }
         }
         return result;
+    }
+	// this method take paramValues_size array and reads all elements get count
+	// number and value of element and store to new created List with information , that
+	// information are String so we return List of Strings
+	public List<String> getAllSize(String[] size_array) {
+        ArrayList<String> sizeList = new ArrayList<String>();
+        if (size_array.length > 0) {
+            int n = 0;
+            for (String temp : size_array) {
+                sizeList.add("number - " + ++n + " is value of " + temp);
+            }
+        } else {
+            sizeList.add("empty");
+        }
+        return sizeList;
     }
 }
